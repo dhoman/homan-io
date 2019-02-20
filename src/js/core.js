01e8e9cb51cd7a0c5b380cd882fb4fef911e7d2f
dhoman <dhoman@huskers.unl.edu>
2019-02-20 01:42:51 -0700

    copied stuff from other repo, navbar still broke

--- src/js/core.js
+++ src/js/core.js
@@ -1,10 +1,111 @@
-// simple button click event handler
-function btnHandler(selector, callback) {
-  var btn = document.querySelector(selector);
-  if(!btn) { return; }
-  btn.addEventListener('click', function(event) {
-    event.preventDefault();
-    callback();
-  }, false);
-}
 
+  var imagePath = "{{ '/images/waterfall.jpg' }}";
+  //var imgContainerEl = document.getElementById( 'homan-img-container' );
+  //var canvasContainerEl = document.getElementById( 'homan-canvas' );
+  
+  function loadImage ( src, callback ) {
+    var imageEl = new Image();
+    imageEl.onload = function () {
+      callback( imageEl );
+    };
+    imageEl.src = src;
+  }
+  function addEvent( obj, type, fn ) {
+    if ( obj.attachEvent ) {
+      obj['e'+type+fn] = fn;
+      obj[type+fn] = function(){obj['e'+type+fn]( window.event );};
+      obj.attachEvent( 'on'+type, obj[type+fn] );
+    } else {
+      obj.addEventListener( type, fn, false );
+    }
+  }
+  function getScrollY() {
+    var scrOfY = 0;
+    if( typeof( window.pageYOffset ) == 'number' ) {
+      //Netscape compliant
+      scrOfY = window.pageYOffset;
+    } else if( document.body && document.body.scrollTop )  {
+      //DOM compliant
+      scrOfY = document.body.scrollTop;
+    } 
+    return scrOfY;
+  }
+  function scale(num, in_min, in_max, out_min, out_max) {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  }
+
+  function timeOutGlitch() {
+    var currentTime = Date.now();
+    if (latestGlitch + 2000 < currentTime) {
+      var sin = Math.sin(currentTime);
+      var cos = Math.cos(currentTime);
+      latestGlitch = currentTime;
+      var tempParams = {
+        amount: scale(sin, -1, 1, 10, 60),
+        iterations: scale(cos, -1, 1, 5, 35),
+        quality: scale(sin, -1, 1, 10, 60),
+        seed: scale(sin, -1, 1, 0, 100)
+      }
+      glitchImage(tempParams);
+    }
+  }
+
+  function setTimeoutForGlitch() {
+    setTimeout(function() {
+      timeOutGlitch();
+      setTimeoutForGlitch();
+    }, Math.random() * 10000);
+  }
+
+  var waterfallContainerEl = document.getElementById( 'waterfall' );
+  
+  function glitchImage(params) {
+      console.log('glitchParams: ' + JSON.stringify(params));
+      latestGlitch = Date.now();
+      loadImage( imagePath, function ( img ) {
+        glitch( params )
+          .fromImage( img )
+          .toDataURL()
+          .then( function( dataURL ) {
+            var imageEl = new Image();
+            //imageEl.class = 'waterfall';
+            imageEl.src = dataURL;
+            //$('.waterfall').replaceWith( imageEl );
+            //$('.waterfall').css('background-image',  'url('+dataURL+')');
+            //var imageEl = new Image();
+            //imageEl.src = dataURL;
+            if(waterfallContainerEl.childNodes.length) {
+              waterfallContainerEl.replaceChild(imageEl, waterfallContainerEl.childNodes[0]);
+            } else {
+              waterfallContainerEl.appendChild(imageEl);
+            }
+          });
+      });
+  }
+
+  addEvent(window, 'scroll', function(event) {
+    var y = getScrollY();
+    if (Math.abs(lastestY - y) > 50) {
+      lastestY = y;
+      var sin = Math.sin(y);
+      var cos = Math.cos(y);
+      var tempParams = {
+        amount: scale(sin, -1, 1, 10, 60),
+        iterations: scale(cos, -1, 1, 5, 35),
+        quality: scale(sin, -1, 1, 10, 60),
+        seed: scale(sin, -1, 1, 0, 100)
+      }
+      glitchImage(tempParams);
+    }
+  });
+  setTimeoutForGlitch();
+  var lastestY = 0;
+  var latestGlitch = Date.now();
+
+  var params = {
+    amount:     20,
+    iterations: 40,
+    quality:    50,
+    seed:       25
+  };
+  glitchImage(params)
---
+++ src/site/posts/practice.md
@@ -0,0 +1,19 @@
+---
+title: untitled part 2
+summary: ramblings
+date: 2019-02-02
+tags:
+  - idk
+layout: layouts/post.njk
+---
+
+## Practice and Learning
+
+I used to be a bad employee, I'm still not a perfect employee by any means, but I used to be afraid to write code and when I wrote code it was never good code.
+
+My code is still far from perfect, but now I'm not nearly as afraid to code, even if I end up throwing away the project or not showing it to anyone...
+
+I think that's a big problem. In the 90s, between xanga, myspace, and geocities, it was perfectly ok to browse your friends profiles that included gifs of the hamster dance, marque tags and what have you.
+
+Today though, while there has been a big of a movement to bring back the atitude of the internet being a playground, there is no big social media sites where one can truly fsck their profile up.
+
